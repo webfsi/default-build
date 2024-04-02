ee7280a67ca0f6a93a66d8efdc708840d1226ff8
ForestSpirit <forestspirit@email.cz>
2024-04-02 17:19:26 +0300

    next step

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@
 // import setFullHeight from './helpers/setFullHeight';
 
 
-
 window.addEventListener('load', () => {
 	// setFullHeight(); //Set VH variable for mobile safari 100VH, use scss mixin fullheight()
 	// commonScripts();
